2532713e70a1fdd85878f7cca6a243dfcbbe61d9
astonmedia <astonmedia@hotmail.com>
2022-12-15 06:32:14 +1000

    finished pages part of the app

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ const App = () => {
                 <Route path="/" element={<Ecommerce />} />
                 <Route path="/ecommerce" element={<Ecommerce />} />
                 {/* Pages */}
-                <Route path="/pages" element={<Orders />} />
+                <Route path="/orders" element={<Orders />} />
                 <Route path="/employees" element={<Employees />} />
                 <Route path="/customers" element={<Customers />} />
                 {/* Apps */}
